57295b7216139c21930899e6f9bc507875fdb927
mlc <mlc@debian>
2014-04-22 11:54:02 -0400

    modified e2-4 code

--- e2-4.cpp
+++ e2-4.cpp
@@ -8,21 +8,18 @@ int main(){
     for (int i = 0; i < n; i++){
         char str1[11];
         cin.getline(str1,11,'\n');
-        for (int k = 0; str1[k] != '\n'; k++)
+        for (int k = 0; str1[k] != '\0'; k++)
                 cnt[str1[k]-'A']++;
     }
-    cout << "finished input" <<endl;
     for (int i = 0; i < p; i++){
         char str2[101];
         cin.getline(str2,101,'\n');
-        cout << str2 << endl;
-        for (int j = 0; str2[j] != '\n'; j++)
+        for (int j = 0; str2[j] != '\0'; j++)
             cnt[str2[j]-'A']--;
     }
-    cout << "finished p" <<endl;
     for (int i = 0; i < 26; i++){
-        for (int j = 0; j < cnt[i]; j++)
-            cout << cnt[i] + 'A';
+	for (int j = 0; j < cnt[i]; j++)
+	    cout << i+'A';
     }
     cout << endl;
     return 0;
